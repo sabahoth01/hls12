1f48dde9d91b54b79aece5a772f0fbc49e2179b5
Brunotte Wasaulua <82088279+sabahoth01@users.noreply.github.com>
2024-02-21 10:34:50 +0300

    Update BookingRepositoryTest.java
    //

--- BookingRepositoryTest.java
+++ BookingRepositoryTest.java
@@ -1,4 +1,77 @@
 package ru.itmo.hls1.repository;
 
-public class BookingRepositoryTest {
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+
+
+@Testcontainers
+@SpringBootTest
+@AutoConfigureMockMvc
+public class BookingRepositoryTest  {
+
+    @Container
+    private static final PostgreSQLContainer<?> POSTGRESQL_CONTAINER = new PostgreSQLContainer<>("postgres:latest")
+            .withDatabaseName("test_db")
+            .withUsername("test_user")
+            .withPassword("test_password");
+
+    @BeforeAll
+    public static void setUp() {
+        postgreSQLContainer.start();
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        postgreSQLContainer.stop();
+    }
+
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testGetAllRecords() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/bookings/")
+                        .param("page", "0")
+                        .param("size", "5")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    public void testGetRecordById() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/bookings/1")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    public void testCreateBookingRecord() throws Exception {
+        String requestJson = "{\"playgroundId\": 1, \"playerId\": 1, \"date\": \"2023-03-15\", \"startTime\": \"10:00\", \"endTime\": \"12:00\"}";
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/bookings/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isCreated());
+    }
+
+    @Test
+    public void testDeleteBookingRecord() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.delete("/bookings/1")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
 }
